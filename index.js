bc768f6f6b62cf8890ac9d7fdca8ec8d1a2ebffc
FabianFN <96866412+FabianFN@users.noreply.github.com>
2022-12-28 16:42:37 -0500

    Fnbot

--- index.js
+++ index.js
@@ -13,11 +13,11 @@ const { name, author } = require(join(__dirname, './package.json'))
 const { say } = cfonts
 const rl = createInterface(process.stdin, process.stdout)
 
-say('FN - Bot\nMD', {
+say('Mystic - Bot\nWhatsApp Bot MD', {
 font: 'chrome',
 align: 'center',
 gradient: ['red', 'magenta']})
-say(`Bot Subcreado por Fablink`, {
+say(`Bot creado por Bruno Sobrino`, {
 font: 'console',
 align: 'center',
 gradient: ['red', 'magenta']})
